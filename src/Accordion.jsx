da3ea43b1329b4862f8dd5cc489d20e4ad98247f
gliese-backup <glieseb+1@proton.me>
2024-11-17 08:04:02 +0000

    Accordion component

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-function Accordion({ title, description, showUnderline = false }) {
-  console.log(showUnderline);
-
+function Accordion({
+  title,
+  description,
+  showUnderline = false,
+  isOpen = false,
+}) {
   return (
-    <details className="w-52 border-2 bg-gray-50 rounded-lg p-2">
+    <details
+      className="w-52 border-2 border-collapse bg-gray-50 p-2"
+      open={isOpen}
+    >
       <summary
         className={`cursor-pointer ${showUnderline ? "hover:underline" : ""}`}
       >
--- src/Bellow.jsx
+++ src/Bellow.jsx
@@ -8,7 +8,14 @@ function Bellow() {
         title="React vs Angular"
         description="This is some text"
         showUnderline
+        isOpen
       />
+      <Accordion
+        title="Flutter vs React Native"
+        description="This is some text"
+        isOpen
+      />
+      <Accordion title="React vs Angular" description="This is some text" />
     </div>
   );
 }
