4bd4dd17ef0118bcae25be4d86b7ffa16bec3fd0
gliese-backup <glieseb+1@proton.me>
2024-12-08 07:22:53 +0000

    Add something

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -5,16 +5,25 @@ function Accordion({
   isOpen = false,
   onClick = () => {},
 }) {
+  const URL = "https://wakati.ritoho8508.workers.dev/";
+  fetch();
+
   return (
     <details
-      className="w-52 border-2 border-collapse bg-gray-50 p-2"
+      className="w-60 border-2 border-collapse bg-gray-50 p-2"
       open={isOpen}
       onToggle={() => onClick()}
     >
       <summary
-        className={`cursor-pointer ${showUnderline ? "hover:underline" : ""}`}
+        className={`cursor-pointer ${
+          showUnderline ? "hover:underline" : ""
+        } flex justify-between items-center`}
       >
         {title}
+
+        <span className="text-xs text-slate-600">
+          {description.split(" ").length} words
+        </span>
       </summary>
       <p className="text-sm">{description}</p>
     </details>
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { useState } from "react";
-import configs from "./assets/data.json";
+import data from "./assets/data.json";
 import Bellow from "./Bellow.jsx";
 
 function App() {
-  const [lastClicked, setLastClicked] = useState("Not clicked");
-
   return (
     <div className="flex flex-col gap-1 items-center justify-center min-h-screen">
-      <h1 className="text-xl font-bold">Last Selected: {lastClicked}</h1>
-      <Bellow
-        data={configs}
-        showUnderline={true}
-        isOpen={false}
-        onClick={(value) => {
-          setLastClicked(value);
-        }}
-      />
+      <Bellow data={data} />
     </div>
   );
 }
