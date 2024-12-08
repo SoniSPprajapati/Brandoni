e0d71e4e03d83fbd6f69e94bec32a84a6aa375ab
Soni Prajapati <prajapatisoni839@gmail.com>
2024-12-08 08:31:35 +0000

    Complete

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 function Accordion({
   title,
   description,
@@ -5,16 +7,39 @@ function Accordion({
   isOpen = false,
   onClick = () => {},
 }) {
+  const [readTime, setReadTime] = useState(0);
+  const URL = "https://wakati.ritoho8508.workers.dev/api/calculate";
+
+  useEffect(() => {
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sentence: description,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setReadTime(data.seconds);
+      });
+  }, []);
+
   return (
     <details
-      className="w-52 border-2 border-collapse bg-gray-50 p-2"
+      className="w-60 border-2 border-collapse bg-gray-50 p-2"
       open={isOpen}
       onToggle={() => onClick()}
     >
       <summary
-        className={`cursor-pointer ${showUnderline ? "hover:underline" : ""}`}
+        className={`cursor-pointer ${
+          showUnderline ? "hover:underline" : ""
+        } flex justify-between items-center`}
       >
         {title}
+
+        <span className="text-xs text-slate-600">{readTime} seconds</span>
       </summary>
       <p className="text-sm">{description}</p>
     </details>
